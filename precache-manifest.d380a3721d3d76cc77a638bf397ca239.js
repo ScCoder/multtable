d86357b98fbad25ec81032ecf64c1545cae61ef1
ScCoder <ser9eiusov@gmail.com>
2020-03-09 17:05:21 +0700

    Updates

---
+++ precache-manifest.d380a3721d3d76cc77a638bf397ca239.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08e31615faf1c3864801430ddd4b5d78",
+    "url": "/multtable/index.html"
+  },
+  {
+    "revision": "0f32517cef7e35860ad7",
+    "url": "/multtable/static/css/main.714232f7.chunk.css"
+  },
+  {
+    "revision": "938d14095276fd61f701",
+    "url": "/multtable/static/js/2.1d240039.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/multtable/static/js/2.1d240039.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0f32517cef7e35860ad7",
+    "url": "/multtable/static/js/main.beeae64d.chunk.js"
+  },
+  {
+    "revision": "0c8548356f26d4cdfc40",
+    "url": "/multtable/static/js/runtime-main.5bd8452e.js"
+  },
+  {
+    "revision": "bfda37f0e45e4e0c42b7442958920f01",
+    "url": "/multtable/static/media/battery.bfda37f0.png"
+  },
+  {
+    "revision": "8a24d45a1ee5663204b771dde18e6d4b",
+    "url": "/multtable/static/media/robot.8a24d45a.png"
+  }
+]);
