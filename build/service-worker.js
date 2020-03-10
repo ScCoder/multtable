612ae4fd54d2dd80cd93b877df8f404617aa90f5
ScCoder <ser9eiusov@gmail.com>
2020-03-10 11:42:06 +0700

    add battery count to display

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c3849ff7dd974dba85942fa4d67fe2ff.js"
+  "/multtable/precache-manifest.d380a3721d3d76cc77a638bf397ca239.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/multtable/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
--- src/components/MultTable/Display/Display.js
+++ src/components/MultTable/Display/Display.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import s from './Display.module.css';
 
-const Display = ({gameActive,startGame,a,b,result,answers}) => {
+const Display = ({gameActive,startGame,a,b,result,answers,batteryCount}) => {
 
     const SatatusItems = Object.keys(answers).map(answKey => <div key={answKey}
       className={s.status_item + ' ' + ((answers[answKey].isCorrect === null) ? s.status_item_none : (answers[answKey].isCorrect) ? s.status_item_ok : s.status_item_failed)} >
@@ -12,6 +12,7 @@ const Display = ({gameActive,startGame,a,b,result,answers}) => {
         <div className={s.status_panel}>
           {SatatusItems}
         </div>
+        <div className = {s.battery_count}>{batteryCount}</div>
         <div className={s.main_panel}> {gameActive ? `${a}x${b}=${result}` : <button className={s.main_panel_button} onClick={startGame}>Начать!</button>} </div>
       </div>
     )
